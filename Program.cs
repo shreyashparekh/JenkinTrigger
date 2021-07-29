2b0e729d3a31e7df0714ebfbe24afde30de7626a
shreyashparekh <shreyashparekh99@gmail.com>
2021-07-29 15:27:36 +0530

    Triggers Training

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JTriggerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jenkins Trigger sikh lo!");
+            Console.WriteLine("Learning jenkins");
+        }
+    }
+}
